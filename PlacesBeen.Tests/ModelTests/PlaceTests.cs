e0abc772ef8230d43f35f66b9b850a6ac2438909
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-29 15:05:08 -0700

    project names changed

---
+++ PlacesBeen.Tests/ModelTests/PlaceTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using PlacesBeen.Models;
+
+namespace PlacesBeen.Tests
+{
+  [TestClass]
+  public class PlaceTests
+  {
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    }
+  }
+}
